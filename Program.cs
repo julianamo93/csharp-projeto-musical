9f2e0a223b097a9c49d06b7d666ced4fa70175ec
= <=>
2024-02-12 11:40:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas Vindas ao Screen Sound!";
+
+void ExibirMensagemDeBoasVindas()
+{ 
+    Console.WriteLine(@"
+    
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝
+
+░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
+
+void ExibirOpcoesDoMenu()
+{
+    Console.WriteLine("\n1. Registrar banda");
+    Console.WriteLine("2. Mostrar Todas as Bandas");
+    Console.WriteLine("3. Avaliar Banda");
+    Console.WriteLine("4. Exibir média da Banda");
+    Console.WriteLine("5. Sair");
+
+    Console.Write("\nEscolha sua opção: ");
+    String opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+  
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 5: Console.WriteLine("Encerrando o programa...");
+            break;
+        default: Console.WriteLine("Opção inválida!");
+            break;
+    }
+}
+
+ExibirMensagemDeBoasVindas();
+ExibirOpcoesDoMenu();
