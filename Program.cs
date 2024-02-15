1a40311d9b384109049dafe8ee1d893959917e9c
= <=>
2024-02-14 22:08:09 -0300

    refatoração 2

--- Program.cs
+++ Program.cs
@@ -48,7 +48,7 @@ void ExibirOpcoesDoMenu()
             break;
         case 3: AvaliarUmaBanda();
             break;
-        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+        case 4: ExibirMedia();
             break;
         case 5: Console.WriteLine("Encerrando o programa...");
             break;
@@ -122,6 +122,31 @@ void AvaliarUmaBanda()
     {
         Console.WriteLine($"A banda {nomeDaBanda} não foi encontrada");
         Console.WriteLine("Digite uma tecla para voltar ao Menu Principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+}
+
+void ExibirMedia()
+{
+    Console.Clear();
+    ExibirTituloDaOpcao("Exibir Média da Banda");
+    Console.Write("Digite o nome da banda que deseja consultar a média: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        List<int> notasDaBanda = bandasRegistradas[nomeDaBanda];
+        Console.WriteLine($"A média da banda {nomeDaBanda} é {notasDaBanda.Average()}");
+        Console.WriteLine("Digite uma tecla para voltar ao Menu Principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+    else 
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada.");
+        Console.WriteLine("Digite qualquer tecla para voltar ao Menu Principal");
         Console.ReadKey();
         Console.Clear();
         ExibirOpcoesDoMenu();
